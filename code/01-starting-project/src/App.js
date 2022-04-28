d9ea63e32d8f79f0ac0ad1d0e16f6c273aa48164
Kyle Jorve <kyle.jorve@simpleviewinc.com>
2022-04-27 17:11:38 -0700

    section 7

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -13,7 +13,10 @@ const App = () => {
 	const addGoalHandler = enteredText => {
 		setCourseGoals(prevGoals => {
 			const updatedGoals = [...prevGoals];
-			updatedGoals.unshift({ text: enteredText, id: 'goal1' });
+			updatedGoals.unshift({
+				text: enteredText,
+				id: `goal${updatedGoals.length + 1}`,
+			});
 			return updatedGoals;
 		});
 	};
